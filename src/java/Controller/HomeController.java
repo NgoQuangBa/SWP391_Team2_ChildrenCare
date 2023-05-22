93da60e3e1334552c4a5bdeecd384bbf729691cf
NguyenThanhTung <133035143+NguyenThanhTung1006@users.noreply.github.com>
2023-05-22 22:37:42 +0700

    Add files via upload

---
+++ src/java/Controller/HomeController.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import DAO.BlogDAO;
+import DAO.SliderDAO;
+import Model.Blog;
+import Model.Slider;
+import java.io.IOException;
+import java.util.List;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "HomeController", urlPatterns = { "/home" })
+public class HomeController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            BlogDAO blogDao = new BlogDAO();
+            List<Blog> list = blogDao.getAll();
+            request.setAttribute("listblogP", list);
+        } catch (Exception ex) {
+            System.out.println("error" + ex.getMessage());
+        }
+        request.getRequestDispatcher("view/home.jsp").forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            BlogDAO blogDao = new BlogDAO();
+            List<Blog> list = blogDao.get6BlogLastest();
+            request.setAttribute("listblogP", list);
+            SliderDAO dao = new SliderDAO();
+            List<Slider> listSlider = dao.getAllSliderShow();
+            request.setAttribute("listSlider", listSlider);
+        } catch (Exception ex) {
+            System.out.println("error" + ex.getMessage());
+        }
+        request.getRequestDispatcher("view/home.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
