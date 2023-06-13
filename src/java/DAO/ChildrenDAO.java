02424fa5e766a4c7ef76c0b0a5d8cc4d4b534e29
DinhQuangCong-he160695 <133000097+DinhQuangCong-he160695@users.noreply.github.com>
2023-06-13 16:07:24 +0700

    Add files via upload

--- src/java/DAO/ChildrenDAO.java
+++ src/java/DAO/ChildrenDAO.java
@@ -5,39 +5,78 @@
  */
 package DAO;
 
-import Context.DBContext;
+import context.DBContext;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import Model.Child;
+import java.util.ArrayList;
+import java.util.List;
+import model.children;
 
-
+/**
+ *
+ * @author admin
+ */
 public class ChildrenDAO {
 
-    public int createReturnId(Child child) {
+    public List<children> getAllChildren() {
+        List<children> list = new ArrayList<>();
         try {
-            String sql = "INSERT INTO [dbo].[Children]\n"
-                    + "           ([ChildName]\n"
-                    + "           ,[Gender]\n"
-                    + "           ,[ParentID])\n"
-                    + "     VALUES\n"
-                    + "           (?,?,?)";
             Connection conn = new DBContext().getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, child.getChildName());
-            ps.setBoolean(2, child.isGender());
-            ps.setInt(3, child.getParent().getUserID());        
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                return rs.getInt(1);
+            String sql = "select * from [category]";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                children chil = new children(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getBoolean(4), rs.getInt(5));
+                list.add(chil);
             }
-        } catch (Exception ex) {
-            Logger.getLogger(ChildrenDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return list;
+        } catch (Exception e) {
         }
-        return 0;
+        return null;
+    }
+
+    public static List<children> getAllChildrenByID(int id) {
+        List<children> list = new ArrayList<>();
+        try {
+            Connection conn = new DBContext().getConnection();
+            String sql = "select * from children where user_id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                children chil = new children(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getBoolean(4), rs.getInt(5));
+                list.add(chil);
+            }
+            return list;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ChildrenDAO chil = new ChildrenDAO();
+        List<children> list = chil.getAllChildrenByID(47);
+        System.out.println(list.get(0).getChildren_name());
+    }
+
+    public void addNewChildren(children c) {
+        try {
+            Connection conn = new DBContext().getConnection();
+            String sql = "INSERT INTO [dbo].[children]\n"
+                    + "           ([user_id]\n"
+                    + "           ,[children_name]\n"
+                    + "           ,[children_gender]\n"
+                    + "           ,[children_age])\n"
+                    + "     VALUES\n"
+                    + "           (?,?,?,(YEAR(GETDATE()))-?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, c.getUser_id());
+            ps.setString(2, c.getChildren_name());
+            ps.setBoolean(3, c.isChildren_gender());
+            ps.setInt(4, c.getChildren_age());
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
     }
 }
