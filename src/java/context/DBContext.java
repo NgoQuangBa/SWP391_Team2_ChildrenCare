2664bbba1be098d00507b0217831c3a1da372768
NgoQuangBa <132918242+NgoQuangBa@users.noreply.github.com>
2023-06-14 22:23:09 +0700

    Create DBContext.java

---
+++ src/java/context/DBContext.java
@@ -0,0 +1,32 @@
+package context;
+
+import Utills.Constance;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+public class DBContext {
+
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+ /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + Constance.SERVERNAME + ":" + Constance.PORTNUMBER
+                + ";databaseName=" + Constance.DATABASENAME;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, Constance.USER_ACCOUNT_DATABASE, Constance.USER_PASS_DATABASE);
+    }
+
+    public static void main(String[] args) {
+        try {
+            DBContext dBContext = new DBContext();
+            if (dBContext.getConnection() != null) {
+                System.out.println("Kết nối thành công");
+            } else {
+                System.out.println("Kết nối thất bại");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
