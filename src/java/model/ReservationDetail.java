ec1290f7af66d8aa97185202f0b91e4a6d73d5c1
DinhQuangTung <134306261+DinhQuangTung-He161511@users.noreply.github.com>
2023-07-13 13:49:46 +0700

    Add files via upload

--- src/java/model/ReservationDetail.java
+++ src/java/model/ReservationDetail.java
@@ -1,7 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
 import java.sql.Date;
 
+/**
+ *
+ * @author win
+ */
 public class ReservationDetail {
     private int reservationDetail_id;
     private int reservation_id;
