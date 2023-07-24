1f4a53d8241220abc728e80ef7db497f2e7b1b19
NgoQuangBa <132918242+NgoQuangBa@users.noreply.github.com>
2023-07-24 23:56:06 +0700

    Update ReservationDetailManagerController.java

--- src/java/staff/ReservationDetailManagerController.java
+++ src/java/staff/ReservationDetailManagerController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller.staff;
 
 import DAO.CategoryDAO;
@@ -20,10 +15,7 @@ import model.ListServiceOfReservation;
 import model.ReservationDetailManager;
 import model.category;
 
-/**
- *
- * @author Admin
- */
+
 public class ReservationDetailManagerController extends HttpServlet {
 
     /**
